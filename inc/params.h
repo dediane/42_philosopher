afb21462ff4a03be61289a0316f394b3adeaacfb
Diane <dianedecourt@gmail.com>
2021-10-14 01:21:21 +0200

    jj

--- inc/params.h
+++ inc/params.h
@@ -1,21 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.c                                            :+:      :+:    :+:   */
+/*   params.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ddecourt <ddecourt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/13 23:24:16 by ddecourt          #+#    #+#             */
-/*   Updated: 2021/10/14 01:10:53 by ddecourt         ###   ########.fr       */
+/*   Created: 2021/10/14 01:09:16 by ddecourt          #+#    #+#             */
+/*   Updated: 2021/10/14 01:20:59 by ddecourt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int ac, char **av)
+#include "../philo.h"
+
+struct s_philo
 {
-	if (ac != 5 || ac != 6)
-	{
-		printf("Usage: ./philo number_of_philosopher time_to_die time_to_eat \
-		time_to_sleep [number_of_times_each_philosopher_must_eat]");
-		return (1);
-	}
-}
+	int id;
+	int state;
+}		t_philo;
--- philo.h
+++ philo.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ddecourt <ddecourt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/13 23:24:45 by ddecourt          #+#    #+#             */
-/*   Updated: 2021/10/13 23:24:54 by ddecourt         ###   ########.fr       */
+/*   Created: 2021/10/14 01:09:46 by ddecourt          #+#    #+#             */
+/*   Updated: 2021/10/14 01:10:58 by ddecourt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,4 @@
 #include <unistd.h>
 #include <sys/time.h>
 #include <pthread.h>
+#include "inc/params.h"
