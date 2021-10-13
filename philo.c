2771e8208f36aac31ed68ffea32dae79c4159d43
Diane <dianedecourt@gmail.com>
2021-10-13 23:34:20 +0200

    philo

--- philo.c
+++ philo.c
@@ -1,13 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philosopher.c                                      :+:      :+:    :+:   */
+/*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ddecourt <ddecourt@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dianedecourt <dianedecourt@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/13 23:03:05 by ddecourt          #+#    #+#             */
-/*   Updated: 2021/10/13 23:03:07 by ddecourt         ###   ########.fr       */
+/*   Created: 2021/10/13 23:24:16 by ddecourt          #+#    #+#             */
+/*   Updated: 2021/10/13 23:32:02 by dianedecour      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int main(int ac, char **av)
+{
+	if (ac != 5 || ac != 6)
+	{
+		printf("Usage: ./philo number_of_philosopher time_to_die time_to_eat \
+		time_to_sleep [number_of_times_each_philosopher_must_eat]");
+		return (1);
+	}
+}
 
--- philo.h
+++ philo.h
@@ -1,13 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philosopher.h                                      :+:      :+:    :+:   */
+/*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ddecourt <ddecourt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/13 23:03:18 by ddecourt          #+#    #+#             */
-/*   Updated: 2021/10/13 23:03:20 by ddecourt         ###   ########.fr       */
+/*   Created: 2021/10/13 23:24:45 by ddecourt          #+#    #+#             */
+/*   Updated: 2021/10/13 23:24:54 by ddecourt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <pthread.h>
