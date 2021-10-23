334b0c8b3ff7782c98e30a56980e81d0e6452952
Diane <dianedecourt@gmail.com>
2021-10-23 22:30:25 +0200

    push

--- inc/params.h
+++ inc/params.h
@@ -6,18 +6,20 @@
 /*   By: ddecourt <ddecourt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/14 01:09:16 by ddecourt          #+#    #+#             */
-/*   Updated: 2021/10/22 19:01:06 by ddecourt         ###   ########.fr       */
+/*   Updated: 2021/10/23 22:27:28 by ddecourt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../philo.h"
+#ifndef PARAMS_H
+# define PARAMS_H
 
 typedef struct s_philo
 {
 	pthread_t		philo;
-	pthread_mutex_t	fork;
-	pthread_mutex_t	next_fork;
-	int				id;
+	pthread_mutex_t	*fork;
+	pthread_mutex_t	*next_fork;
+	pthread_mutex_t	*mutex_write;
+	unsigned int	id;
 	long			init_time;
 	long			t_wait;
 }		t_philo;
@@ -31,3 +33,5 @@ typedef struct s_env
 	long	t_to_eat;
 	long	t_to_sleep;
 }		t_env;
+
+#endif
--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: ddecourt <ddecourt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/20 18:12:23 by ddecourt          #+#    #+#             */
-/*   Updated: 2021/10/22 19:31:48 by ddecourt         ###   ########.fr       */
+/*   Updated: 2021/10/23 22:29:39 by ddecourt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,43 +16,38 @@ void	*routine(void *ph)
 {
 	pthread_mutex_lock(ph);
 	pthread_mutex_unlock(ph);
-	return(0);
+	return (0);
 }
 
 void	init_my_philos(t_philo *ph, t_env *env, int nb)
 {
-	int i;
+	int				i;
+	pthread_mutex_t	*next_fork;
 
 	i = -1;
-	while(++i < nb)
+	while (++i < nb)
 	{
-		pthread_mutex_t next_fork;
 		ph->id = i + 1;
 		if (&(ph[i].fork) == NULL && i != (env->nb_philo - 1))
 		{
-			pthread_mutex_init(&(ph[i].fork), NULL);
-			pthread_mutex_init(&next_fork, NULL);
 			ph[i + 1].fork = next_fork;
 			ph[i].next_fork = next_fork;
 		}
 		if (i == (env->nb_philo - 1))
 		{
-			pthread_mutex_init(&(ph[i].fork), NULL);
-			pthread_mutex_init(&next_fork, NULL);
 			ph[0].fork = next_fork;
 			ph[i].next_fork = next_fork;
 		}
 		env->start_time = 0;
-		pthread_create(&ph[i].philo, NULL, &routine, &ph);
 	}
 }
 
-int	main(int ac, char** av)
+int	main(int ac, char **av)
 {
-	t_philo *ph;
-	t_env env;
-	int nb_of_philo;
-	
+	t_philo	*ph;
+	t_env	env;
+	int		nb_of_philo;
+
 	nb_of_philo = ft_atoi(av[1]);
 	ph = malloc(sizeof(t_philo) * (nb_of_philo + 1));
 	if (!ph)
