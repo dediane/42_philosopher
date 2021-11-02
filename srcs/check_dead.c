653ee8e34040a5eb211275c048e290235e963c49
dediane <ddecourt@student.42.fr>
2021-11-02 18:54:57 +0100

    almost not broken

--- srcs/check_dead.c
+++ srcs/check_dead.c
@@ -6,7 +6,7 @@
 /*   By: ddecourt <ddecourt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 17:13:28 by ddecourt          #+#    #+#             */
-/*   Updated: 2021/11/02 18:10:13 by ddecourt         ###   ########.fr       */
+/*   Updated: 2021/11/02 18:53:59 by ddecourt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ int	check_dead(t_philo *ph)
 	while (++i < ph->env->nb_philo)
 	{
 		pthread_mutex_lock(&ph->env->mutex_write);
-		if ((get_timestamp(ph->env->start_time) - ph[i].last_meal) > (long)ph[i].env->t_to_die)
+		if ((get_timestamp(ph[i].env->start_time) - ph[i].last_meal) > (long)ph[i].env->t_to_die)
 		{
 			ft_print_status(ph, "died");
 			ph->env->is_dead = 1;
