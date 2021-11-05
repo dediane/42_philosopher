f7271928a9a9b3b0b48e80b7f27386a2af76690c
dediane <ddecourt@student.42.fr>
2021-11-05 17:33:42 +0100

    fixing NORMINETTE

--- srcs/check_dead.c
+++ srcs/check_dead.c
@@ -12,6 +12,29 @@
 
 #include "../inc/philo.h"
 
+void	eating_enough_time(t_philo *ph)
+{
+	ph->env->is_dead = 1;
+	pthread_mutex_unlock(&ph->env->eating);
+	pthread_mutex_unlock(&ph->env->death_mutex);
+}
+
+void	check_dead2(t_philo *ph)
+{
+	ft_print_status(ph, "died");
+	pthread_mutex_lock(&ph->env->death_mutex);
+	ph->env->is_dead = 1;
+	pthread_mutex_unlock(&ph->env->death_mutex);
+	pthread_mutex_unlock(&ph->env->eating);
+}
+
+void	check_dead3(t_philo *ph)
+{
+	pthread_mutex_lock(&ph->env->death_mutex);
+	ph->env->is_dead = 0;
+	pthread_mutex_unlock(&ph->env->death_mutex);
+}
+
 int	check_dead(t_philo *ph)
 {
 	int	i;
@@ -27,30 +50,14 @@ int	check_dead(t_philo *ph)
 			j++;
 			pthread_mutex_lock(&ph[i].env->death_mutex);
 			if (j == ph->env->nb_philo)
-			{
-				ph[i].env->is_dead = 1;
-				pthread_mutex_unlock(&ph[i].env->eating);
-				pthread_mutex_unlock(&ph[i].env->death_mutex);
-				return (1);
-			}
+				return (eating_enough_time(&ph[i]), 1);
 			pthread_mutex_unlock(&ph[i].env->death_mutex);
 		}
 		if ((get_timestamp(ph[i].env->start_time) - ph[i].last_meal) > \
 		(long)ph[i].env->t_to_die)
-		{
-			ft_print_status(ph, "died");
-			pthread_mutex_lock(&ph[i].env->death_mutex);
-			ph[i].env->is_dead = 1;
-			pthread_mutex_unlock(&ph[i].env->death_mutex);
-			pthread_mutex_unlock(&ph[i].env->eating);
-			return (1);
-		}
+			return (check_dead2(&ph[i]), 1);
 		else
-		{
-			pthread_mutex_lock(&ph[i].env->death_mutex);
-			ph->env->is_dead = 0;
-			pthread_mutex_unlock(&ph[i].env->death_mutex);
-		}
+			check_dead3(&ph[i]);
 		pthread_mutex_unlock(&ph[i].env->eating);
 	}
 	return (0);
